1199c82590e565c56921e53be7f6d8d66b48636c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-02 17:56:01 +0200

    solution_EF : récupération du challenge depuis le servuer.

--- Elgamal_forgery/solution_EF.py
+++ Elgamal_forgery/solution_EF.py
@@ -1,3 +1,21 @@
+from client import Server
+
 if __name__ == "__main__" :
     print("Programme Elgamal Forgery ...")
+    server = Server("http://pac.bouillaguet.info/TP4/ElGamal-forgery/")
+
+    challenge = server.query("PK/verkyndt")
+
+    p = challenge['p']
+    g = challenge['g']
+    h = challenge['h']
+
+
+    print(p)
+    print(h)
+    print(g)
+
+
+
+
     print("Fermeture du programme.")
