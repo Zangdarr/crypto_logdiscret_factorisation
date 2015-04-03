319ab948fa21eb0a8a09d46be8cfc56bc0d17599
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-03 15:01:41 +0200

    solution_EF : ajout des fonctions de calcul du XGCD et du modulo inverse.

--- Elgamal_forgery/solution_EF.py
+++ Elgamal_forgery/solution_EF.py
@@ -1,4 +1,23 @@
 from client import Server
+import random
+
+def XGCD(a,b):
+  u = (1, 0) #representation de a
+  v = (0, 1) #representation de b
+
+  while b!= 0:
+    q,r = divmod(a, b)
+    a = b
+    b = r
+    tmp = (u[0] - q*v[0], u[1] - q*v[1])
+    u = v
+    v = tmp
+  return a, u[0], u[1]
+
+def modinv(a, b):
+  g, x, y = XGCD(a,b)
+  return x
+
 
 if __name__ == "__main__" :
     print("Programme Elgamal Forgery ...")
